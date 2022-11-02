80c08edf61120a3b54ad5886e43957264be797ed
Iniro <95286107+Iniro@users.noreply.github.com>
2022-11-02 12:45:24 +0100

    Update Test2.sol

--- Test2.sol
+++ Test2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.sdfsdfsdfsdfsdfsdf0;
 
 import "../interfaces/IERC721.sol";
 import "../interfaces/IERC721TokenReceiver.sol";
