7e46d82694625f73dbf83d93ae55d706f84cfc1b
Iniro <95286107+Iniro@users.noreply.github.com>
2022-11-02 12:20:32 +0100

    Update Reginald.js

--- Reginald.js
+++ Reginald.js
@@ -1,3 +1,3 @@
-if (!/\[[A-Z]+-[0-9]+\]\s/.test(reginald.pr.title)) {
-  reginald.error("Title should start with the ticket nuber e.g.: [JIRA-123]")
+if (reginald.git.modifiedFiles.some(value => value.endsWith('Reginaldfile.js'))) {
+   reginald.warning("Reginaldfile was modified");
 }
